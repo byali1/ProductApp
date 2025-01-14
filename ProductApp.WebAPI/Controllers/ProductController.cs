f68ab7551743aeb41e5ab3d243bbeafa22c568f3
ali.ilgin <byali.ilgin@gmail.com>
2025-01-14 13:31:45 +0300

    Add project files.

---
+++ ProductApp.WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductApp.Application.Interfaces.Repository;
+
+namespace ProductApp.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            var products = await _productRepository.GetAllAsync();
+
+
+            return Ok(products);
+        }
+    }
+}
---
+++ src/Infrastructure/ProductApp.Persistence/ServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ProductApp.Application.Interfaces.Repository;
+using ProductApp.Persistence.Context;
+using ProductApp.Persistence.Repositories;
+
+namespace ProductApp.Persistence
+{
+    public static class ServiceRegistration
+    {
+
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+
+            //Db
+            services.AddDbContext<ApplicationDbContext>(opt => opt.UseInMemoryDatabase("MemoryDb"));
+
+            services.AddScoped<IProductRepository, ProductRepository>();
+        }
+    }
+}
